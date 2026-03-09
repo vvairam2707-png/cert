96a44f5ebd55e40e408f9895b442a4276bc52ee3
User <user@example.com>
2026-03-09 22:10:04 +0530

    fix: update CORS for production

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
